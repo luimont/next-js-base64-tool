780af793dc7a2f0584a52c6e519e330868bf6070
Luis Montenegro <luismontenegro.1995@gmail.com>
2024-02-04 20:12:51 -0300

    Set arrow function

--- pages/providers.tsx
+++ pages/providers.tsx
@@ -2,7 +2,7 @@
 
 import { ThemeProvider } from "next-themes"
 
-export function Providers({children}: {children: React.ReactNode}){
+export const Providers = ({children}: {children: React.ReactNode}) => {
   return (
     /* Este provider es necesario para evitar el parpadeo al recargar la pagina y obtener el tema */
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
