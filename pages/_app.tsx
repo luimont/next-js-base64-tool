cdad43cd99905e62591b7e31e32db7c0f3261b49
Luis Montenegro <luismontenegro.1995@gmail.com>
2024-02-04 20:19:02 -0300

    Fix Provider name

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@
 
 import "@/styles/globals.css"
 import type { AppProps } from "next/app"
-import { Providers } from "./providers"
+import { Providers } from "./Provider"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
