7f340ff613c01a504e3bb17cf90a6c4c430d48e2
Luis Montenegro <luismontenegro.1995@gmail.com>
2024-02-04 21:04:48 -0300

    Remove href

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
   return (
     <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800 md:w-[800px] lg:w-[900px]">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {currentYear} <a href="https://flowbite.com/" className="hover:underline">Base64 Tool</a>. <em>By Luis Montenegro. </em>All Rights Reserved.
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {currentYear} <a href="#" className="hover:underline">Base64 Tool</a>. <em>By Luis Montenegro. </em>All Rights Reserved.
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
